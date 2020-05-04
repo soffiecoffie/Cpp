eb7ee568f85b4e9ef17b506560f5b7c20d8a2372
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-04 22:47:54 +0300

    Update MeetingTime.h

--- My Calender/MeetingTime.h
+++ My Calender/MeetingTime.h
@@ -2,7 +2,7 @@
 #define MEETINGTIME_H
 #include "Time.h"
 
-
+//структура, която събира началото и края на дадено събитие/среща в едно
 struct MeetingTime {
 	Time start;			//should it stay as a structure?
 	Time end;
