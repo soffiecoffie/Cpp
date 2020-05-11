7bdf5cd77468496cb737c7033fc0893e8ac1c58f
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-12 01:01:18 +0300

    Update Arrangement.h

--- My Calender/Arrangement.h
+++ My Calender/Arrangement.h
@@ -3,26 +3,35 @@
 #include "Date.h"
 #include "MeetingTime.h"
 
-class Arrangement{
-	char* name;
+class Arrangement {
+	bool holiday = false;
+	char* name; 
 	char* note; 
 	Date day;
-	MeetingTime time; 
+	MeetingTime time;
 
 public:
 	Arrangement() : name(nullptr), note(nullptr) {}; //shte se izvikat defaultnite na date i meetingtime right?
-	Arrangement(const char*, const char*, Date, MeetingTime);
+	Arrangement(const char*, const char*, const Date&, const MeetingTime&);
+	Arrangement(const Date&, const MeetingTime&);
 	~Arrangement();
 
 	Date getDay() const { return day; };
-	MeetingTime getTime() const{ return time; };
+	MeetingTime getTime() const { return time; };
 	char* getName() const { return name; };
 	char* getNote() const { return note; };
+	bool getHoliday() const { return holiday; };
+	size_t getLength();	//TODO
 
 	void setName(const char*);
 	void setNote(const char*);
+	void setHoliday(bool);
+	void setDate(const Date&);
+	void setStartTime(const Time&);
+	void setEndTime(const Time&);
 	void print() const;
 
+	Arrangement& operator=(const Arrangement&); //needed?
 
 	std::istream& read(std::istream&);
 	std::ostream& write(std::ostream&) const;
