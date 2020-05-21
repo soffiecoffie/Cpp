9f0bf34bc09061aeb5cdc0caa5b72c291702cbd6
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-22 02:29:29 +0300

    Update Column.h

--- Database/Column.h
+++ Database/Column.h
@@ -6,42 +6,52 @@
 template<typename T>
 class Column : public IColumn
 {
+
+//	T* Cells<T> cells;
 	size_t size;
+	T* cells;		//TODO MAKE A CLASS SO ISNULL CAN HAPPEN
 	std::string title;
-	T* cells;
+	std::string type;
 	
 public:
-	Column(const char*, const T*, size_t);
-	
+	Column(const std::string&, const std::string&, const T*, size_t);
+	Column(const char*);
 	Column();
+
 	~Column();
 
-	void printCellLine() const;
-	void printCellTitle() const;
-	void printCellInsides(size_t) const;
-	void printCell() const;
-
-	void printPadding(size_t, size_t) const;
-	size_t getColSize() const;
-
-//	void setNullCell(size_t);
-//	template<typename U>
-//	friend void printCentered(const U);
-
-	void addCell(T);
-	void setTitle(const char*);
 	size_t getCellLength(size_t) const;
-	size_t getLongest() const;
-	void printMany(const char*, size_t) const; 
-	void printCellContents(size_t) const;
-	void print() const;
-	//void printCentered(T) const;
-	//void printCentered(const char*) const;
 
+	std::ostream& printCellLine(std::ostream&) const;
+	std::ostream& printCellTitle(std::ostream&) const;
+	std::ostream& printCellInsides(size_t, std::ostream&) const;
+
+	std::ostream& printMany(const char*, size_t, std::ostream&) const;
+	std::ostream& printPadding(size_t, size_t, std::ostream&) const;
+
+//	std::string removePaddingFromStr(std::string); //когато чета колоната думата която записвам има 
+//											//празно място от двете страни за центриране което не трябва да има
+//	bool isSpaceBetweenWords(std::string, size_t);
+											//void printPadding(size_t, size_t) const;
+	size_t getColSize() const;
+	std::string getType() const;
+	std::string getTitle() const;
+	std::string getCellAt(size_t) const;
+	
+	void addCell(std::string);
+
+	void setTitle(std::string);
+	void setType(const char*);
+	size_t getLongest() const;
+	void printCell(size_t) const;
+	void print() const;
+	int digits(int) const;
+	bool isOdd(int) const;
+	void setNullCellAt(size_t);
 };
 
-int digits(int);
-bool isOdd(int);
+	//	template<typename U>
+	//	friend void printCentered(const U);
 
 #include "Column.hpp" 
 #endif
