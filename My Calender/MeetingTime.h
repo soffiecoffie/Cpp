e462df1d53ad48c0a5fba83539bddacecc3a76ad
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-22 02:26:13 +0300

    Update MeetingTime.h

--- My Calender/MeetingTime.h
+++ My Calender/MeetingTime.h
@@ -1,6 +1,8 @@
 #ifndef MEETINGTIME_H
 #define MEETINGTIME_H
 #include "Time.h"
+#include <string>
+#include <cassert>
 
 //структура, която събира началото и края на дадено събитие/среща в едно
 struct MeetingTime {
@@ -8,7 +10,7 @@ struct MeetingTime {
 	Time end;
 
 	MeetingTime() : start(Time()), end(Time()) {};
-	MeetingTime(Time start, Time end) : start(start), end(end) {};
+	MeetingTime(Time start, Time end) : start(start), end(end) { assert(start < end); };
 	bool operator==(const MeetingTime& other)const { return start == other.start && end == other.end; }
 };
 
