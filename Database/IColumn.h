b6740a6434553a670e3274a6a3a5e9f439e43747
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-22 02:30:21 +0300

    Update IColumn.h

--- Database/IColumn.h
+++ Database/IColumn.h
@@ -1,16 +1,29 @@
 #ifndef ICOLUMN__H
 #define ICOLUMN__H
-
+#include <string>
 
 class IColumn
 {
 
 public:
-	virtual void printCellLine() const = 0;
-	virtual void printCellTitle() const = 0;
-	virtual void printCellInsides(size_t) const = 0;
-	virtual void printCell() const = 0;
+	virtual void addCell(std::string) = 0;
+
+	virtual std::ostream& printCellLine(std::ostream&) const = 0;
+	virtual std::ostream& printCellTitle(std::ostream&) const = 0;
+	virtual std::ostream& printCellInsides(size_t, std::ostream&) const = 0;
+
 	virtual size_t getColSize() const = 0;
+	virtual std::string getType() const = 0;
+	virtual std::string getTitle() const = 0;
+	virtual void printCell(size_t) const = 0;
+
+	virtual std::string getCellAt(size_t) const = 0;
+
+	virtual void setNullCellAt(size_t) = 0;
+
+	virtual void setType(const char*) = 0;
+	virtual void setTitle(std::string) = 0;
+	
 };
 
 #endif
