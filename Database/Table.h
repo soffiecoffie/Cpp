b14fecc30e25c618a80037a295fec6cf837812ca
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-22 02:29:05 +0300

    Update Table.h

--- Database/Table.h
+++ Database/Table.h
@@ -1,21 +1,34 @@
 #ifndef TABLE_H
 #define TABLE_H
 
-#include "IColumn.h"
+//#include "IColumn.h"
+#include "Column.h"
+#include <string>
 
 class Table
 {
 	size_t size;
 	IColumn** cols;
+	std::string name;
 
 public:
 	Table();
-	Table(IColumn* [], size_t s);
+	Table(IColumn* [], size_t s, const std::string&);
+	~Table();
 
-	void print() const;
-	void printTableLine() const;
-	void printTableTitles() const;
-	void printTableRow() const;
+	std::ostream& print(std::ostream& out) const;
+	std::ostream& printTableLine(std::ostream& out) const;
+	std::ostream& printTableTitles(std::ostream& out) const;
+	std::ostream& printTableRow(std::ostream& out) const;
+
+	std::istream& read(std::istream&);
+	std::ostream& write(std::ostream&) const;
+	
+	std::string removePaddingFromStr(std::string s);
+	bool isSpaceBetweenWords(std::string str, size_t ind);
+
+	size_t getMaxColSize() const;
+
 };
 
 #endif
