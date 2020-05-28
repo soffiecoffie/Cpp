e81061ab9ae2fdd41acba4bdc4c0d7d9bf97afb9
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-29 01:20:57 +0300

    Update Column.hpp

--- Database/Column.hpp
+++ Database/Column.hpp
@@ -2,51 +2,87 @@
 #include <iostream>
 #include <cassert>
 #include "Column.h"
+#include "Utils.h"
 
+//конструктор с параметри
 template<typename T>
-Column<T>::Column(const std::string& _title, const std::string& _type, const T* _cells, size_t _size) : size(_size), cells(new T[_size])
+Column<T>::Column(const std::string& _title, const std::string& _type, const T* _cells, size_t _size)
+				 : size(0), cells(nullptr), title(_title)
 {
-	title = _title;
-	type = _type;
-	
-	for (size_t i = 0; i < size; ++i)
-	{
-		cells[i] = _cells[i];
-	}
+	setType(_type);
+	setCells(_cells, _size);
 }
 
+//конструктор с параметър
 template<typename T>
 Column<T>::Column(const char* type): size(0), title(""), cells(nullptr)
 {
 	setType(type);
 }
 
+//конструктор по подразбиране
 template<typename T>
 Column<T>::Column() : size(0), title(""), type(""), cells(nullptr)
 {
 }
 
+//деструктор
 template<typename T>
 Column<T>::~Column()
 {
+	del();
+}
+
+//изтрива динамичната памет
+template<typename T>
+void Column<T>::del()
+{
+	for (size_t i = 0; i < size; ++i)
+	{
+		delete cells[i];
+	}
 	delete[] cells;
+}
+
+//добавя още едно място към масива
+template<typename T>
+void Column<T>::addSpace()
+{
+	T* temp = new T[size];
+	for (size_t i = 0; i < size; ++i)
+	{
+		temp[i] = *cells[i];
+	}
+
+	del();
+
+	cells = new T * [++size];
+	for (size_t i = 0; i < size - 1; ++i)
+	{
+		cells[i] = new T;
+		*cells[i] = temp[i];
+	}
+
+	delete[] temp;
 }
 
 //връща броя на цифрите в клетката
 template<typename T>
 size_t Column<T>::getCellLength(size_t ind) const
 {
-	if (cells[ind] == NULL) return 4;
-	return digits(cells[ind]);
+	if (cells[ind] == nullptr) return 4;
+	return digits(*cells[ind]);
 }
 
 //връща броя на символите в клетката
 template<>
 inline size_t Column<std::string>::getCellLength(size_t ind) const
 {
-	return cells[ind].size();
+	if (cells[ind] == nullptr) return 4;
+	return cells[ind]->size();
 }
 
+//принтира линията на клетката
 template<typename T>
 std::ostream& Column<T>::printCellLine(std::ostream& out) const
 {
@@ -56,235 +92,159 @@ std::ostream& Column<T>::printCellLine(std::ostream& out) const
 	return out;
 }
 
+//принтира заглавието на клетката
 template<typename T>
 std::ostream& Column<T>::printCellTitle(std::ostream& out) const
 {
 	size_t length = getLongest() + 6;
 	size_t pad = length - title.size();
 	out << "|";
-
-	printPadding(title.size(), pad, out);
+	
+	if (isOdd(pad)) printPadding(pad + 1, out);
+	else printPadding(pad, out);
 
 	out << title;
 
-	printPadding(title.size(), pad, out);
+	printPadding(pad, out);
 	return out;
 }
 
+//принтира съдържанието на клетката
 template<typename T>
-std::ostream& Column<T>::printCellInsides(size_t i, std::ostream& out) const 
+std::ostream& Column<T>::printCellContents(size_t i, std::ostream& out) const 
 {
-	size_t length = getLongest() + 6; //to do longest with null
+	size_t pad, length = getLongest() + 6;		//+6 е за празното място около съдържанието на клетката (мин. 3 " " от двете страни)
 
-	if (cells[i] == NULL) {
-		size_t pad = length - 4;
+	if (cells[i] == nullptr || i == -1) {		
+		pad = length - 4;
 		out << "|";
 
-		printPadding(4, pad, out);
+		if (isOdd(pad)) printPadding(pad + 1, out); 
+		else printPadding(pad, out);
 
 		out << "NULL";
-
-		if (isOdd(pad))pad = pad - 1;
-		printPadding(4, pad, out);
 	}
 	else {
-		size_t pad = length - getCellLength(i);
+		pad = length - getCellLength(i);
 
 		out << "|";
 
-		printPadding(getCellLength(i), pad, out);
+		if (isOdd(pad)) printPadding(pad + 1, out); 
+		else printPadding(pad, out);
 
-		out << cells[i];
-
-		if (isOdd(pad))pad = pad - 1;
-		printPadding(getCellLength(i), pad, out);
+		out << *cells[i];
 	}
 
+	printPadding(pad, out);
+	
 	return out;
 }
 
-template<>
-inline std::ostream& Column<std::string>::printCellInsides(size_t i, std::ostream & out) const
-{
-	size_t length = getLongest() + 6;
-	size_t pad = length - getCellLength(i);
-
-	out << "|";
-
-	printPadding(getCellLength(i), pad, out);
-
-	out << cells[i];
-
-	if (isOdd(pad))pad = pad - 1;
-	printPadding(getCellLength(i), pad, out);
-
-	return out;
-}
-
+//принтира празно място
 template<typename T>
-std::ostream& Column<T>::printPadding(size_t length, size_t pad, std::ostream& out) const
+std::ostream& Column<T>::printPadding(size_t pad, std::ostream& out) const
 {
-	if (isOdd(length)) {
-		printMany(" ", (pad + 1) / 2, out);
-	}
-	else printMany(" ", pad / 2, out);
+	printMany(" ", pad / 2, out);
 	return out;
 }
 
+//връща големината на колоната
 template<typename T>
 size_t Column<T>::getColSize() const
 {
 	return size;
 }
 
+//връща низ с типа на колоната
 template<typename T>
-inline std::string Column<T>::getType() const
+std::string Column<T>::getType() const
 {
 	return type;
 }
 
+//връща заглавието на колоната
 template<typename T>
-inline std::string Column<T>::getTitle() const
+std::string Column<T>::getTitle() const
 {
 	return title;
 }
 
+//връща низ от стойността на дадена клетка
 template<typename T>
 std::string Column<T>::getCellAt(size_t ind) const
 {
-	if (cells[ind] == NULL) return "NULL";
-	return std::to_string(cells[ind]);
+	if (cells[ind] == nullptr) return "NULL";
+	return std::to_string(*cells[ind]);
 }
 
+//връща низ от стойността на дадена клетка на колона от тип std::string
 template<>
 inline std::string Column<std::string>::getCellAt(size_t ind) const
 {
-	return cells[ind];
+	return *cells[ind];
 }
 
+//връща масив от указатели към стойностите на колоната
+template<typename T>
+T** Column<T>::getCells() const
+{
+	return cells;
+}
+
+//добавя нова клетка
 template<typename T>
 void Column<T>::addCell(std::string cell)
 {
-	T* temp = new T[size];
-	for (size_t i = 0; i < size; ++i)
-	{
-		temp[i] = cells[i];
-	}
-	delete[] cells;
+	addSpace();
 
-	cells = new T[++size];
-	for (size_t i = 0; i < size - 1; ++i)
-	{
-		cells[i] = temp[i];
-	}
-	cells[size - 1] = cell;
+	cells[size - 1] = new T(cell);
 }
 
+//добавя нова клетка в колона от тип int
 template<>
 inline void Column<int>::addCell(std::string cell)
 {
-	int* temp = new int[size];
-	for (size_t i = 0; i < size; ++i)
-	{
-		temp[i] = cells[i];
-	}
-	delete[] cells;
+	addSpace();
 
-	cells = new int[++size];
-	for (size_t i = 0; i < size - 1; ++i)
-	{
-		cells[i] = temp[i];
-	}
-	cells[size - 1] = std::stoi(cell);
+	cells[size - 1] = new int(std::stoi(cell)); 
 }
 
+//добавя нова клетка в колона от тип double
 template<>
 inline void Column<double>::addCell(std::string cell)
 {
-	double* temp = new double[size];
-	for (size_t i = 0; i < size; ++i)
-	{
-		temp[i] = cells[i];
-	}
-	delete[] cells;
+	addSpace();
 
-	cells = new double[++size];
-	for (size_t i = 0; i < size - 1; ++i)
-	{
-		cells[i] = temp[i];
-	}
-	cells[size - 1] = std::stod(cell);
+	cells[size - 1] = new double(std::stod(cell));
 }
-//template<typename T>
-//void Column<T>::addCell(std::string cell)
-//{
-//	if (type == "string") {
-//
-//		T* temp = new T[size];
-//		for (size_t i = 0; i < size; ++i)
-//		{
-//			temp[i] = cells[i];
-//		}
-//		delete[] cells;
-//
-//		cells = new T[++size];
-//		for (size_t i = 0; i < size - 1; ++i)
-//		{
-//			cells[i] = temp[i];
-//		}
-//		cells[size - 1] = cell;
-//	}
-//	if (type == "double") {
-//		T* temp = new T[size];
-//		for (size_t i = 0; i < size; ++i)
-//		{
-//			temp[i] = cells[i];
-//		}
-//		delete[] cells;
-//
-//		cells = new T[++size];
-//		for (size_t i = 0; i < size - 1; ++i)
-//		{
-//			cells[i] = temp[i];
-//		}
-//		cells[size - 1] = std::stod(cell);
-//	}
-//	if (type == "int") {
-//		T* temp = new T[size];
-//		for (size_t i = 0; i < size; ++i)
-//		{
-//			temp[i] = cells[i];
-//		}
-//		delete[] cells;
-//
-//		cells = new T[++size];
-//		for (size_t i = 0; i < size - 1; ++i)
-//		{
-//			cells[i] = temp[i];
-//		}
-//		cells[size - 1] = std::stoi(cell);
-//	}
-//
-//
-//}
 
-
-//template<typename T>
-//void Column<T>::setNullCell(size_t ind)
-//{
-//	cells[ind] = NULL;
-//}
-
+//задава стойност на заглавието
 template<typename T>
-void Column<T>::setTitle(std::string t)
+void Column<T>::setTitle(const std::string& t)
 {
 	title = t;
 }
 
+//задава стойност на масива от указатели
 template<typename T>
-void Column<T>::setType(const char* t)
+void Column<T>::setCells(const T* arr, size_t s)
 {
-	assert(strcmp(t, "int") || strcmp(t, "string") || strcmp(t, "double"));
+	del();
+
+	size = s;
+	
+	cells = new T* [s];
+	for (size_t i = 0; i < s; ++i)
+	{
+		cells[i] = new T(arr[i]);
+	}
+}
+
+//задава стойност на типа на колоната
+template<typename T>
+void Column<T>::setType(const std::string& t)
+{
+	assert(t == "int" || t == "string" || t == "double");
 	type = t;
 }
 
@@ -305,12 +265,7 @@ size_t Column<T>::getLongest() const
 	return getCellLength(max);
 }
 
-template<typename T>
-void Column<T>::printCell(size_t ind) const
-{
-	std::cout << cells[ind];
-}
-
+//принтира колоната
 template<typename T>
 void Column<T>::print() const
 {
@@ -323,7 +278,7 @@ void Column<T>::print() const
 	for (size_t i = 0; i < size; ++i)
 	{
 		std::cout << "+\n";
-		printCellInsides(i, std::cout);
+		printCellContents(i, std::cout);
 		std::cout << "|\n";
 		printCellLine(std::cout);
 	}
@@ -331,39 +286,9 @@ void Column<T>::print() const
 	std::cout << "\n";
 }
 
-//извежда на екрана даден символ по даден брой пъти един след друг
-template<typename T>
-std::ostream& Column<T>::printMany(const char* ch, size_t n, std::ostream& out) const
-{
-	for (size_t i = 0; i < n; ++i)
-	{
-		out << ch;
-	}
-	return out;
-}
-
-template<typename T>
-int Column<T>::digits(int n) const
-{
-	if (n == 0)	return 0;
-	return 1 + digits(n / 10);
-}
-
-template<typename T>
-bool Column<T>::isOdd(int n) const
-{
-	return n % 2 != 0;
-}
-
+//задава стойността на дадена клетка да бъде nullptr
 template<typename T>
 void Column<T>::setNullCellAt(size_t ind)
 {
-	cells[ind] = NULL;
-}
-
-template<>
-inline void Column<std::string>::setNullCellAt(size_t ind)
-{
-	assert(ind < size);
-	cells[ind] = "NULL";
+	cells[ind] = nullptr;
 }
