a5b7dff49426bdd2a7880ee5a2ac13d638eb6d02
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-29 01:19:46 +0300

    Add files via upload

---
+++ Database/Utils.cpp
@@ -0,0 +1,37 @@
+﻿#include "Utils.h"
+#include <iostream>
+#include <cassert>
+
+//връща броя на цифрите на дадено число
+int digits(int n)
+{
+	if (n == 0)	return 0;
+	return 1 + digits(n / 10);
+}
+
+//връща истина ако подаденото число е нечетно
+bool isOdd(int n) 
+{
+	return n % 2 != 0;
+}
+
+//извежда на екрана даден символ по даден брой пъти
+std::ostream& printMany(const char* ch, size_t n, std::ostream& out)
+{
+	for (size_t i = 0; i < n; ++i)
+	{
+		out << ch;
+	}
+	return out;
+}
+
+
+//проверява дали празното място на подадения индекс в дадения низ се намира между думи
+bool isSpaceBetweenWords(std::string str, size_t ind)
+{
+	assert(str[ind] == ' ');
+	if (ind != str.size() - 1 && ind != 0) {
+		return (str[ind - 1] != ' ') && (str[ind + 1] != ' ');
+	}
+	return false; //ако е първия или последния символ 
+}
---
+++ Database/Utils.h
@@ -0,0 +1,15 @@
+﻿#ifndef UTILS__H
+#define UTILS__H
+#include <fstream>
+#include <string>
+
+//Полезни функции
+
+int digits(int);
+bool isOdd(int);
+std::ostream& printMany(const char*, size_t, std::ostream&);
+bool isSpaceBetweenWords(std::string, size_t);
+
+
+
+#endif 
