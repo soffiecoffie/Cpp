a7cad81886d0bce3b4f7ec6370c65ce02e951bb3
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-29 01:21:52 +0300

    Update Column.h

--- Database/Column.h
+++ Database/Column.h
@@ -7,15 +7,19 @@ template<typename T>
 class Column : public IColumn
 {
 
-//	T* Cells<T> cells;
 	size_t size;
-	T* cells;		//TODO MAKE A CLASS SO ISNULL CAN HAPPEN
+	T** cells;
 	std::string title;
 	std::string type;
-	
+
+private:
+	void del();
+	void addSpace();
+
 public:
 	Column(const std::string&, const std::string&, const T*, size_t);
 	Column(const char*);
+	Column(const Column&) = delete;
 	Column();
 
 	~Column();
@@ -24,34 +28,25 @@ public:
 
 	std::ostream& printCellLine(std::ostream&) const;
 	std::ostream& printCellTitle(std::ostream&) const;
-	std::ostream& printCellInsides(size_t, std::ostream&) const;
+	std::ostream& printCellContents(size_t, std::ostream&) const;
 
-	std::ostream& printMany(const char*, size_t, std::ostream&) const;
-	std::ostream& printPadding(size_t, size_t, std::ostream&) const;
+	std::ostream& printPadding(size_t, std::ostream&) const;
 
-//	std::string removePaddingFromStr(std::string); //когато чета колоната думата която записвам има 
-//											//празно място от двете страни за центриране което не трябва да има
-//	bool isSpaceBetweenWords(std::string, size_t);
-											//void printPadding(size_t, size_t) const;
 	size_t getColSize() const;
 	std::string getType() const;
 	std::string getTitle() const;
 	std::string getCellAt(size_t) const;
+	T** getCells() const;
 	
 	void addCell(std::string);
 
-	void setTitle(std::string);
-	void setType(const char*);
+	void setTitle(const std::string&);
+	void setCells(const T*, size_t);
+	void setType(const std::string&);
 	size_t getLongest() const;
-	void printCell(size_t) const;
 	void print() const;
-	int digits(int) const;
-	bool isOdd(int) const;
 	void setNullCellAt(size_t);
 };
-
-	//	template<typename U>
-	//	friend void printCentered(const U);
 
 #include "Column.hpp" 
 #endif
