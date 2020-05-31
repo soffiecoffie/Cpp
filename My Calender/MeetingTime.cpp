f19bda3698d05d581f097cb865774b9f42c823f2
soffiecoffie <63425040+soffiecoffie@users.noreply.github.com>
2020-05-31 10:40:44 +0300

    Update MeetingTime.cpp

--- My Calender/MeetingTime.cpp
+++ My Calender/MeetingTime.cpp
@@ -1,17 +1,1 @@
 ﻿#include "MeetingTime.h"
-
-////връща време за среща от подаден низ
-//MeetingTime MeetingTime::stringToMeetingTime(std::string str)
-//{
-//	MeetingTime result;
-//	std::string start = str;
-//	start.erase(start.begin(), start.begin() + 5);
-//	//result.setHour(std::stoi(h));
-//
-//	std::string end = str;
-//	end.erase(end.begin(), end.end() - 2);
-//	result.setMinutes(std::stoi(m));
-//
-//	return result;
-//}
-//
